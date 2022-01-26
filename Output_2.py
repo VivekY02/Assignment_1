da0376156ad5c02bf329c4bceb0087c738879370
VivekY02 <97078657+VivekY02@users.noreply.github.com>
2022-01-26 10:03:20 +0530

    Add files via upload

---
+++ Output_2.py
@@ -0,0 +1,65 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:\Users\JL YADAV\AppData\Local\Programs\Python\Python37-32\Assignment 2.py 
+Question1
+Enter the string : Python is a case sensitive language
+The length of the given string is :  35
+The string in reverse would be :   egaugnal evitisnes esac a si nohtyP
+The new string becomes :   a case sensitive
+The new substring is : object oriented
+The replaced string will be :  Python is  object oriented language
+The first occurence of the given substring "a" is at index no. = 10
+The inputted strings with no white spaces will be "
+ Pythonisacasesensitivelanguage
+
+
+##########################################################################################
+
+
+Question 2
+
+Hey, Vivek Yadav here! 
+My SID is 21105009 
+I am from ECE and my CGPA is 9
+
+
+#####################################################################################
+
+
+Question 3
+ a&b :  8
+ a|b :  58
+ a^b :  50
+a<<2 :  224 	b<<2 : 40
+a>>2 :  14 	b>>2 : 2
+
+
+##################################################################################
+
+
+Question 4
+Enter 1st no. : 56
+Enter 2nd no. : 47
+Enter 3rd no. : 50
+Highest no. = 56
+
+
+##################################################################################
+
+
+Qustion 5
+Enter input string : My name is vivek
+Yes , 'name' is present in the string
+
+
+################################################################################
+
+
+Question 6
+
+Enter the length of side : 13
+Enter length of 2nd side : 12
+Enter length of 3rd side : 5
+Yes , it can form a triangle
+>>> 
