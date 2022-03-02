2d73351c5ffa449f8bd1a55c4bcc6033db43c618
VivekY02 <97078657+VivekY02@users.noreply.github.com>
2022-03-02 10:57:20 +0530

    Add files via upload

---
+++ output4.py
@@ -0,0 +1,90 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:\Users\JL YADAV\AppData\Local\Programs\Python\Python37-32\assginment 4.py 
+-----------------------------------------------------------------
+Question 1
+
+The problem of tower of Hanoi with three disks.
+
+Move Disk 1 from A to C
+Move Disk 2 from A to B
+Move Disk 1 from C to B
+Move Disk 3 from A to C
+Move Disk 1 from B to A
+Move Disk 2 from B to C
+Move Disk 1 from A to C
+
+------------------------------------------------------------------
+Question 2
+ 
+ 
+Enter the number of rows in Pascal's Triangle = 6
+
+Using recursions =
+
+          1   
+        1   1   
+      1   2   1   
+    1   3   3   1   
+  1   4   6   4   1   
+1   5   10   10   5   1   
+
+Using loops =
+
+          1   
+        1   1   
+      1   2   1   
+    1   3   3   1   
+  1   4   6   4   1   
+1   5   10   10   5   1   
+
+-------------------------------------------------------------------
+Question 3
+
+ 
+ 
+Enter the first integer = 5
+Enter the second integer = 4
+Remainder is =  1
+Quotient is =  1
+Both values are non zero
+{5}
+{5}
+{5, 6}
+{5, 6}
+{5, 6, 7}
+{5, 6, 7}
+{5, 6, 7}
+Immutable set =  frozenset({5, 6, 7})
+Largest value in the set =  7
+Hash value =  7
+
+-------------------------------------------------------------------
+Question 4
+
+ 
+ 
+Object created
+The name of the student is Vivek Yadav and SID is 21105009.
+Object destroyed
+
+-------------------------------------------------------------------
+Question 5
+
+ 
+a. The updated salary of the employee Mehak is 70000
+b. Employee Viren record deleted
+ 
+Question 6
+
+
+Enter the first word: teach
+Enter a new meaningful word to test your friendship: cheat
+
+Does the word makes sense?(y or n)
+y
+The friends pass the friendship test!!
+
+
+>>> 
